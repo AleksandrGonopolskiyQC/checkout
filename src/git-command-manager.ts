8a8f68ff03d10d7c1fc9e33696ffcaeeaed4fd94
Alex Gonopolskiy <63728183+AleksandrGonopolskiyQC@users.noreply.github.com>
2020-05-29 20:16:30 +0200

    Update git-command-manager.ts

--- src/git-command-manager.ts
+++ src/git-command-manager.ts
@@ -168,7 +168,7 @@ class GitCommandManager {
   }
 
   async fetch(refSpec: string[], fetchDepth?: number): Promise<void> {
-    const args = ['-c', 'protocol.version=2', 'fetch']
+    const args = ['fetch']
     if (!refSpec.some(x => x === refHelper.tagsRefSpec)) {
       args.push('--no-tags')
     }
